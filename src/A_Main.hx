9a698cb5a83799e5994ec0cbcb288d6419349429
Jörg Wasmeier <j.wasmeier@faba.de>
2015-08-18 08:01:25 +0200

    01 Basic Test ------------- Create the first testpage: http://joergwasmeier.de/haxe-meets-angular-01-basic-test

--- src/A_Main.hx
+++ src/A_Main.hx
@@ -1,9 +1,25 @@
 package ;
+import angular.service.Scope;
+import angular.Angular;
+
 class A_Main {
-    public static function main(){
+    /*
+        Haxe start point
+     */
+    static function main() {
+        trace("Start");
+
         new A_Main();
     }
 
     public function new() {
+
+        // App Controller
+        function appController(scope:Scope) {
+            scope.set("data", { appName : "Hallo Welt"});
+        };
+
+        // App Module
+        var module = Angular.module("myModule", []).controller("AppController", appController);
     }
 }
